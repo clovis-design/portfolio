a24fa4d71faf13eaeb399d7014556d297d34ca92
clovis <clovis.sanceo@yahoo.com>
2021-07-03 14:07:05 +0200

    add comestic text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,15 @@ import { ProjectModalType } from './services/project-modal.service'
 })
 export class AppComponent {
     contentProjectHeadphone = {
-        title: 'cloclo',
+        title: 'Construction puzzle bois 3D',
         description:
-            'Ceci est un casque que j’ai réparé grâce à l’impression 3D et à la soudure. Après on peut constater la réalisation de différentes machines fonctionnelles faites en bois. La dernière image vous montre une réalisation faite avec des composants arduino.',
-        imageNames: ['machine-bois.JPG', 'train-bois.JPG', 'arduino.JPG'],
+            '<ul><li>Système locomotive miniature</li><li>Système montagne russe avec billes</li><li>Assemblage sans colle </li><li>Découpage au laser sur contreplaqué fin </li><li>Puzzles à engrenages mécanique</li><li>Fonctionnement à manivelle</li></ul>',
+        imageNames: [
+            'machine-bois.JPG',
+            'billes2.jpg',
+            'train-bois.JPG',
+            'loco3.jpg',
+        ],
     }
     contentProjectScratch = {
         title: 'scratch',
@@ -28,29 +33,65 @@ export class AppComponent {
             'jeux-poisson.png',
         ],
     }
-    contentProjectObjet3D = {
-        title: 'scratch',
+
+    contentProjectObjet = {
+        title: "Réparation d'objets quotidiens",
         description:
-            'Voici la présentation des différentes impressions que j’ai pu réaliser grâce aux imprimantes 3D. Sur ces quelques photos, vous pouvez voir différentes réalisations. Ce cochon a un atout qui est de pouvoir contenir des objets de petite taille comme de la monnaie ou de la matière flexible, cette demi-figurine de deadpool peut contribuer à ajouter une touche de décoration dans votre demeure.  Cet éléphant à la fonctionnalité d’avoir la trompe flexible et les pattes amovibles, quant à elle, cette figurine possède le pouvoir de se démembrer, car elle a été fabriquée en plusieurs pièces séparément. Cette poignée de couvercle a pu avoir une utilité dans le quotidien de la cuisine. La dernière impression est un Yoda fait de résine verte transparente pour le moment il est encore qu’en phase de test.',
+            '<b>Réparation bouton de cellule du chauff-eau Trumatic</b><ul><li>Prototype premier test </li><li>Prototypage par Fusion 360</li><li>PLA blanc</li><li>Temps de réalisation: 2h00</li><li>Impression par imprimante CR_ 10</li></ul><b>Réparation attaches écouteurs</b><ul><li>Fil PLA bleu</li><li>Fichier 3D sur thingivers</li><li>Temps réalisation: 1h00</li><li>Temps de soudure des fils du casque: 2h00</li></ul><b>Réparation poignée couvercle poele</b><ul><li>Fichier sur thingiverse </li><li>Temps de réalisation:2h30</li><li>PLA blanc</li><li>Installation par vis et colle sur support vitre </li></ul>',
+        imageNames: [
+            'objet4.jpg',
+            'objet3D2.JPG',
+            'objet3D1.JPG',
+            'objet3D3.JPG',
+            'repair-csaque-min.JPG',
+            'objet1.jpg',
+            'objet2.jpg',
+            'couvercle2.jpg',
+            'couvercle.JPG',
+        ],
+    }
+
+    contentProjectPot = {
+        title: 'Pot en lévitation',
+        description:
+            '<ul><li>Réalisation en imprimante 3D </li><li>Pot design High Tech</li><li>Fil PLA blanc</li><li>Temps de réalisation du pot: 2h00</li><li>Appareillage magnétique en attente</li><li>ystème permettant la flottaison d’objets</li><li>Objet tournant à 360°</li></ul>',
+        imageNames: [
+            'pot1.jpg',
+            'pot2.jpg',
+            'pot3.jpg',
+            'pot-lévitation-min.JPG',
+        ],
+    }
+
+    contentProjectObjet3D = {
+        title: "Présentation d'Objets 3D",
+        description:
+            'Voici la présentation des différentes impressions que j’ai pu réaliser grâce aux imprimantes 3D. Sur ces quelques photos, vous pouvez voir différentes réalisations. Ce cochon a un atout qui est de pouvoir contenir des objets de petite taille comme de la monnaie ou de la matière flexible, cette demi-figurine de deadpool peut contribuer à ajouter une touche de décoration dans votre demeure.  Cet éléphant à la fonctionnalité d’avoir la trompe flexible et les pattes amovibles, quant à elle, cette figurine possède le pouvoir de se démembrer, car elle a été fabriquée en plusieurs pièces séparément. ',
         imageNames: [
             'cochon.JPG',
             'deadpool.JPG',
             'éléphant.JPG',
             'figurine-girl.JPG',
-            'couvercle.JPG',
             'yoda-résine.JPG',
         ],
     }
 
     contentProjectVSCode = {
-        title: 'scratch',
+        title: 'VSCode',
         description:
             'L’image n°1 représente le code du site sur lequel vous êtes en train de naviguer.Celle-ci montre la partie html du site c’est-à-dire toute la partie visuelle. L’image n°2 vous montre l’un des fichiers du site web nommé fundex ( https://fundex-27fc5.web.app/ ) qui a été réalisé par mes soins. Le code que vous voyez est le “type script” plus communément appelé TS. Il gère toute la partie logistique d’un site web exemple : le fait d’appuyer sur un bouton et que celui-ci nous redirige vers une autre page.',
         imageNames: ['code-portfolio.png', 'fundex.png'],
     }
 
+    contentProjectImprimantePLA = {
+        title: 'Imprimante 3D caractéristiques',
+        description:
+            "<ul><li>Matériels utilisés (PLA, ABS, TPU, Cuivre, Bois, Fibre de Carbone, Gradient Color etc…)</li><li>Logiciels utilisés (PROE, Solid-works, UG, 3d Max, logiciel de conception 3D Rhino, etc…)</li><li>Logiciels d'exploitation (CURA, Repetier-Host, simplifie 3D)</li><li>Système opérationnel (Linux, Windows, OSX)</li></ul>",
+        imageNames: ['imp1.jpg', 'imprimante-3D.JPG', 'imp2.jpg'],
+    }
+
     contentProjectImprimante = {
-        title: 'scratch',
+        title: 'Imprimante 3D résine et son matériel',
         description:
             'Voici mon imprimante 3D en résine de la marque Anycubic du modèle Photon. Elle permet de réaliser des impressions en résine de petite taille.Son fonctionnement est assez simple, elle utilise la lumière UV qui passe par un écran LCD, celui-ci va créer une forme et laissera passer la lumière UV et entraînera la solidification de la résine. Mais il faut faire attention au danger des gaz produits par la résine voilà pourquoi il est obligatoire de porter un masque et des gants afin d’éviter des démangeaisons et des intoxications. Après avoir imprimé votre objet, il faudra procéder à un durcissement par les lumières UV.  le durcir de nouveau par les lumières UV, et le nettoyer. Pour ce faire, on a la possibilité d’utiliser la machine Wash and Cure de chez Anycubic ou de Elegoo . Son fonctionnement est lui aussi très simple, car il y a deux modes wash (laver) et cure (soigner) et un minuteur. Il vous suffit juste de choisir le mode et d’indiquer le temps nécessaire pour votre objet.',
         imageNames: [
